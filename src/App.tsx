ccdca9a38456d91aa0069da1c75d1c90ce5cff43
bozomj <bozomj@gmail.com>
2022-06-24 23:52:32 -0300

    app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ApolloProvider, gql, useQuery } from "@apollo/client"
+import { Router } from "./Router"
+import { client } from './lib/apollo';
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
+
+export default App
